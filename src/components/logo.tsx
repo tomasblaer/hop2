85d6b66495c9d1fc8f9aad82dd8ee234c1149580
mikkimatt <mim23@hi.is>
2024-04-12 15:09:01 +0000

    Smá lógó fix + favicon

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,20 +1,16 @@
-import Link from "next/link"
-import Image from "next/image"
-import localFont from "next/font/local"
+import Link from "next/link";
+import Image from "next/image";
+import localFont from "next/font/local";
 
-import { cn } from "@/lib/utils"
-
-
-
+import { cn } from "@/lib/utils";
 
 export const Logo = () => {
-    return (
-        <Link href="/">
-            <div className="items-center gap-x-2 hidden md:flex p-2 rounded-md w-fit">
-                <p className="text-lg font-extrabold text-white pb-1">
-                 📦 Varningur
-                </p>
-            </div>
-        </Link>
-    )
-}
+  return (
+    <Link href="/">
+      <div className="items-center gap-x-2 hidden md:flex p-2 rounded-md w-fit">
+        <Image src="/logo.svg" alt="Kassi" width={30} height={30} />
+        <p className="text-lg font-extrabold text-white">Varningur</p>
+      </div>
+    </Link>
+  );
+};
--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
         <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
             <Logo />
             <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-                <Button size="sm" variant="outline" asChild>
+                <Button size="sm" asChild>
                     <Link href="/sign-in">
                     Innskráning
                     </Link>
