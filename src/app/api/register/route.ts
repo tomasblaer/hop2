acf7da680d111b9f778e2b861be0a58850c7fbd5
tomasblaer <tomasblaer@gmail.com>
2024-04-12 11:27:42 +0000

    iffy company reg draft

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  return new Response(JSON.stringify(body));
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/nav/header";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Header />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
