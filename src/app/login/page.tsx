302c7a59547a3a88348586835061895ee72a628a
tomasblaer <tomasblaer@gmail.com>
2024-04-15 23:31:39 +0000

    header/footer a reg og login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,18 @@
+import { Footer } from "@/components/footer/footer";
 import CompanyLogin from "@/components/forms/company-login";
+import Header from "@/components/nav/header";
 import { WavyBackground } from "@/components/ui/wavy-background";
 
 export default async function Login() {
   return (
-    <WavyBackground>
-      <main className="flex w-screen h-screen justify-center">
-        <CompanyLogin />
-      </main>
-    </WavyBackground>
-  )
-
+    <>
+      <Header />
+      <WavyBackground>
+        <main className="flex w-screen h-screen justify-center">
+          <CompanyLogin />
+        </main>
+      </WavyBackground>
+      <Footer />
+    </>
+  );
 }
